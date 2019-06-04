87384bcc68635e0287d0d26751115663c8437c8f
srijadvlb <srijadv66@gmail.com>
2019-06-04 01:32:45 -0400

    Test constant

---
+++ DataValues/TestConstant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataValues;
+
+/**
+ *
+ * @author srija
+ */
+public class TestConstant {
+    public static String url="https://www.weightwatchers.com/us/";
+    public static String title1="WW (Weight Watchers): Weight Loss & Wellness Help";
+    public static String title2="Find WW Studios & Meetings Near You | WW USA";
+    public static String zipCode="10011";
+    public static String chromePath=System.getProperty("user.dir")+"//chromedriver.exe";
+    public static String nameOfDay="MON";
+}
